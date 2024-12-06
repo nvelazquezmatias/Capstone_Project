bc393f8e07e06fa8d81878126a35c5a4d754fd18
nguyenlea1 <hnguyen94@fordham.edu>
2024-12-06 12:28:01 -0500

    new scripts

---
+++ capstone__/farm assets/farm food/leek.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+@export var fall_speed: float = 250  # Speed at which the carrot falls
+@export var leek_sprite: Texture 
+@export var leek_name: String = "Leek"
+
+signal leek_eaten
+
+func _on_Leek_body_entered(body):
+	if body.is_in_group("alien"):  # Check if the body is the alien
+		queue_free()  # dissapear
+		emit_signal("leek_eaten") 
+
+# Called every frame
+func _process(delta):
+	position.y += fall_speed * delta
+	
+	if position.y > 600:  
+		queue_free()
--- capstone__/farm assets/farmscene.gd
+++ capstone__/farm assets/farmscene.gd
@@ -1,41 +1,60 @@
-extends Node2D
-
 @export var food_scene_dict: Dictionary  
 @export var spawn_rate: float = 2.0      
 @export var alien: Node2D                
+@export var health_bar: ProgressBar     
+
 var spawn_timer: Timer                   
-var score: int = 0                       
+var score: int = 0                     
+var health: int = 100                 
+var health_decrease_rate: int = 5        
 
 func _ready():
 	spawn_timer = Timer.new()
-	add_child(spawn_timer)  # Add the timer to the scene
+	add_child(spawn_timer)  
 	spawn_timer.wait_time = spawn_rate  # Set the time interval for food spawning
 	spawn_timer.autostart = true  # Start the timer immediately
 	spawn_timer.connect("timeout", self, "_on_SpawnTimer_timeout")
 	
 	if alien == null:
-		alien = get_node("Alien")  # Adjust this path if necessary
+		alien = get_node("Node2D") 
 
+	if health_bar == null:
+		health_bar = get_node("ProgressBar") 
+
+	health_bar.max_value = 100  # Set max health value
+	health_bar.value = health   
+
+	food_scene_dict = {
+		"Carrot": preload("res://farm assets/farm food/carrot.tscn"),
+		"Pumpkin": preload("res://farm assets/farm food/pumpkin.tscn"),
+		"Tomato": preload("res://farm assets/farm food/tomato.tscn"),
+		"Leek": preload("leek"),
+		"Cauliflower": preload("res://path_to_foods/Cauliflower.tscn"),
+		"Eggplant": preload("res://path_to_foods/Eggplant.tscn"),
+		"Corn": preload("res://path_to_foods/Corn.tscn")
+	}
+		
 func _on_SpawnTimer_timeout():
 	# Choose a random food type from the dictionary
 	var food_type = food_scene_dict.keys()[randi() % food_scene_dict.size()]  # Randomly pick a food
 	var food_scene = food_scene_dict[food_type]  # Get the scene for the selected food
 	var food = food_scene.instantiate()  # Create a new instance of the food
 
-	# Spawn the food at a random position on top of the screen
 	var random_x = randf_range(0, get_viewport().size.x)  # Random X position
 	food.position = Vector2(random_x, -food.texture.get_height())  # Spawn above the screen
-	add_child(food)  # Add food to the scene
+	add_child(food) 
 
-	# Connect the signal from the food to handle when it is eaten
 	food.connect("food_eaten", self, "_on_FoodEaten")
+
+func _on_FoodEaten(food_name: String):
+	health -= health_decrease_rate
+	if health < 0:
+		health = 0  
 	
-	func _on_FoodEaten(food_name: String):
-	# Increase the score
-	#score += 1
-	#print(food_name + " eaten! Score: " + str(score))
+	health_bar.value = health
 
+	score += 1
+	print(food_name + " eaten! Score: " + str(score))
 
-
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+	if health == 0:
+		game_over()
