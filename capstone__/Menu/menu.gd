7b42a7e7bea9e8920e6c9ed63154c0ebe1525c9d
nvelazquezmatias <143722340+nvelazquezmatias@users.noreply.github.com>
2024-12-05 22:06:45 -0500

    12/05

--- capstone__/Menu/menu.gd
+++ capstone__/Menu/menu.gd
@@ -12,4 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_texture_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Farm Opening Scene/farm_openingscene.tscn") # Replace with function body.
+	get_tree().change_scene_to_file("res://Intro/Intro.tscn")
